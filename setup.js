2f2ee55e1f4be6d442aafa7acbb8c871759ad453
filip.kostecki <kostecki@devanddeliver.com>
2023-10-11 00:03:34 +0200

    fix: output path

--- setup.js
+++ setup.js
@@ -11,7 +11,7 @@ const BASE_URL = dirname(fileURLToPath(import.meta.url));
 
 
 const handleOrm = async (orm, projectName) => {
-  const outputPath = `${BASE_URL}/${projectName}/package.json`;
+  const outputPath = `${projectName}/package.json`;
   const basePackageJson = BASE_URL+'/base/package.json';
   console.log(orm, projectName)
   if (orm === ORMS.MONGOOSE) {
